0d65ef2a2e7b8b102a44ba218af0492a068d8b2d
Joe Marzullo <jmarzu13@gmail.com>
2016-07-28 10:30:08 -0700

    Angular is working

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require('path');
 var app = express();
 
 app.use(require('morgan')('dev'));
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  // res.send('Hello Backend!');
   res.sendFile(__dirname + '/views/main.html');
 });
+
+app.use('/main', require('./controllers/main'));
 
 var server = app.listen(process.env.PORT || 3000);
 
