41700c55bb6063ce63cc5ff075872d17b07d4c6f
Joe Marzullo <jmarzu13@gmail.com>
2016-07-28 10:05:38 -0700

    Setting up routes

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(require('morgan')('dev'));
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', function(req, res) {
-  res.send('Hello Backend!');
+  // res.send('Hello Backend!');
+  res.sendFile(__dirname + '/views/main.html');
 });
 
 var server = app.listen(process.env.PORT || 3000);
